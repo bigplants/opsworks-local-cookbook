7284cff17fb8a94588a4127376891e7800e8b7cd
bigplants <hirakatad@isao.co.jp>
2014-04-05 23:14:11 +0900

    change metadata #2

--- newrelic/metadata.rb
+++ newrelic/metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Escape Studios'
 maintainer_email 'dev@escapestudios.com'
 license 'MIT'
 description 'Installs/Configures New Relic'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.2'
 
 %w({ debian ubuntu redhat centos fedora scientific amazon windows smartos }).each do |os|
